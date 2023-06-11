98387f42dd326219c5fdc61ab4146c6f1893ee8e
srinanoo <srinanoo@gmail.com>
2023-06-11 20:38:55 +0530

    mongoose - 11-06-2023

--- controllers/traineeControllers.js
+++ controllers/traineeControllers.js
@@ -38,15 +38,14 @@ const addTrainee = async (req, res) => {
     const Trainee = new TraineeModel(req.body);
 
     try {
-        // TraineeModel.find({"username": req.body.username})
-        //     .then(async trainees => {
-        //         if(trainees.length > 0)
-        //             res.json("Trainee Already Exists!")
-        //         else {
-                    await Trainee.save();
-                    res.json("Trainee Added Successfully!");
-            //     }
-            // });
+        let Trainees = await TraineeModel.find({"username": req.body.username});
+            
+        if(Trainees.length > 0)
+            res.json("Trainee Already Exists!")
+        else {
+            await Trainee.save();
+            res.json("Trainee Added Successfully!");
+        }
     } catch (err) {
         let errorList = [];
         if(err.errors) {
@@ -59,11 +58,37 @@ const addTrainee = async (req, res) => {
 };
 
 const updateTrainee = (req, res) => {
-    res.send("Update Trainee - from Routes");
+    // res.send("Update Trainee - from Routes");
+
+    try {
+        TraineeModel.updateOne({"_id": req.body._id}, {$set: req.body})
+            .then(results => {
+                if(results.modifiedCount > 0) {
+                    res.json("Trainee Updated Successfully!");
+                } else {
+                    res.json("Unable to update the Trainee!");
+                }
+            });
+    } catch (err) {
+        res.json(err.message);
+    }
 };
 
 const deleteTrainee = (req, res) => {
-    res.send("Delete Trainee - from Routes");
+    // res.send("Delete Trainee - from Routes");
+
+    try {
+        TraineeModel.deleteOne({"_id": req.body._id})
+            .then(results => {
+                if(results.deletedCount > 0) {
+                    res.json("Trainee Deleted Successfully!");
+                } else {
+                    res.json("Unable to delete the Trainee!");
+                }
+            });
+    } catch (err) {
+        res.json(err.message);
+    }
 };
 
 module.exports = {
